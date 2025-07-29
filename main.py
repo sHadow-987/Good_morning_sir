c9bf96ede99bdaa6c53d14534ce24f3608dff735
sHadow-987 <aliyankhan25552@gmail.com>
2025-07-29 21:34:33 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+
+
+#making bot
+bot_name = "__pibby__"
+centered = bot_name.center(70)
+print(centered)
+
+#Greeting 
+print("hello i am your new asistant pibby please leme know your name!! (^0^)")
+enter_name = input("please enter your name: ")
+entered_name = enter_name.strip()
+
+# main logic 
+struct_time = time.localtime()
+rtime = struct_time.tm_hour
+
+
+if rtime >= 12 and rtime <= 15:
+    print(f"good afternoon {entered_name} !! (^0^) \n {time.asctime(struct_time)}")
+
+elif rtime < 12 :
+    print(f"Good morning {entered_name}!! (^0^) \n {time.asctime(struct_time)} ")
+
+elif rtime >= 17 and rtime <= 19:
+    print(f"good evening {entered_name}!! (^0^) \n {time.asctime(struct_time)}")
+
+elif  rtime >=22:
+    print(f"Its getting late dont you wanna sleep.......(-_-) \n {time.asctime(struct_time)}")
